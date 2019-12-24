2d38c8e5b5910c2aa9468451fdc42c42254d44d5
aandragion <aanmachrus@gmail.com>
2019-12-24 20:03:08 +0700

    Update studiomdl.php

--- application/models/admin/studiomdl.php
+++ application/models/admin/studiomdl.php
@@ -2,44 +2,54 @@
 
 class studiomdl extends CI_Model
 {
-    private $_table = "kursi";//nama tabel
-// nama kolom di tabel, harus sama huruf besar dan huruf kecilnya!
-    public $id_kursi;
-    public $Kursi;
+  private $_table = "kursi";//nama tabel
+  // nama kolom di tabel, harus sama huruf besar dan huruf kecilnya!
+  public $id_kursi;
+  public $Kursi;
 
 
-    public function rules()
-    {
-        return [
-            ['field' => '$id_kursi',
-            'label' => '$id_kursi',
-            'rules' => 'required'],
-
-            ['field' => '$Kursi',
-            'label' => '$Kursi',
-            'rules' => 'required'],
-        ];
-    }
-
-    public function getAll()
-    {
-      $this->db->select('*');
-      $this->db->from('kursi');
-      $query = $this->db->get();
-      return $query->result();
-  }
-  public function tampil_nonton()
+  public function rules()
   {
-    return $this->db->get('nonton')->result();
-}
-// public function tampil_jadwal()
-// {
-//     return $this->db->get('jadwal')->result();
-// }
+    return [
+      ['field' => '$id_kursi',
+      'label' => '$id_kursi',
+      'rules' => 'required'],
 
-public function getById($id)
-{
+      ['field' => '$Kursi',
+      'label' => '$Kursi',
+      'rules' => 'required'],
+    ];
+  }
+
+  public function getAll()
+  {
+    $this->db->select('*');
+    $this->db->from('kursi');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function tampil_data()
+  {
+    $this->db->select('*');
+    $this->db->from('jadwal');
+    $this->db->join('film','film.id_film=jadwal.id_film');
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  function pencarian_d($id= '5db923d93d4'){
+    $this->db->select('*');
+    $this->db->from('nonton');
+      $this->db->join('pesan','pesan.id_pesan=nonton.id_pesan');
+    $this->db->where("pesan.id_jadwal",$id);
+    $query = $this->db->get();
+    return $query->result();
+  }
+
+  public function getById($id)
+  {
     return $this->db->get_where($this->_table, ["$id_kursi" => $id])->row();
-}
+  }
 
 }
