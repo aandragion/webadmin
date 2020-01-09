55ea60977ed3e0c9d5a987c10f75a437308cbe76
aandragion <aanmachrus@gmail.com>
2020-01-09 21:18:31 +0700

    Update db_bioskop.sql

--- db_bioskop.sql
+++ db_bioskop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Des 2019 pada 12.43
+-- Waktu pembuatan: 09 Jan 2020 pada 15.17
 -- Versi server: 10.1.32-MariaDB
 -- Versi PHP: 5.6.36
 
@@ -70,9 +70,9 @@ CREATE TABLE `film` (
 --
 
 INSERT INTO `film` (`id_film`, `judul_film`, `sinopsis`, `trailer`, `gambar`, `id_genre`, `status_film`, `durasi`, `total_view`, `rilis`) VALUES
-('23', 'Black Panther', 'After the events of Captain America: Civil War, Prince T\'Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country\'s new king. However, T\'Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakandan special forces, to prevent Wakanda from being dragged into a world war.', '23.MP4', '5d085ca3518d9.jpg', 'G015', 'playing', '134 menit', '8.3', '2018-02-14'),
-('5dbd905aae457', 'Avengers: Endgame', 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\'s actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...', '5dbd905aae457.MP4', '5dbd905aae457.jpg', 'G001', 'selesai', '181 menit', '2.0', '24 April 2019'),
-('5dd14965e5497', 'Jumanji: The Next Level', 'The gang is back but the game has changed. As they return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored, from the arid deserts to the snowy mountains, in order to escape the world\'s most dangerous game.', '5dd14965e5497.MP4', '5dd14965e5497.jpg', 'G001', 'coming soon', '114 menit', '0.0', '2019-12-04');
+('23', 'Black Panther', 'After the events of Captain America: Civil War, Prince T\'Challa returns home to the reclusive, technologically advanced African nation of Wakanda to serve as his country\'s new king. However, T\'Challa soon finds that he is challenged for the throne from factions within his own country. When two foes conspire to destroy Wakanda, the hero known as Black Panther must team up with C.I.A. agent Everett K. Ross and members of the Dora Milaje, Wakandan special forces, to prevent Wakanda from being dragged into a world war.', 'dxWvtMOGAhw', '5d085ca3518d9.jpg', 'G001', 'playing', '134', '8.5', '2018-02-14'),
+('5dbd905aae457', 'Avengers: Endgame', 'After the devastating events of Avengers: Infinity War (2018), the universe is in ruins due to the efforts of the Mad Titan, Thanos. With the help of remaining allies, the Avengers must assemble once more in order to undo Thanos\'s actions and undo the chaos to the universe, no matter what consequences may be in store, and no matter who they face...', 'TcMBFSGVi1c', '5dbd905aae457.jpg', 'G001', 'playing', '181', '2.0', '2019-04-24'),
+('5dd14965e5497', 'Jumanji: The Next Level', 'The gang is back but the game has changed. As they return to Jumanji to rescue one of their own, they discover that nothing is as they expect. The players will have to brave parts unknown and unexplored, from the arid deserts to the snowy mountains, in order to escape the world\'s most dangerous game.', 'rBxcF-r9Ibs', '5dd14965e5497.jpg', 'G001', 'coming soon', '114', '0.0', '2019-12-04');
 
 -- --------------------------------------------------------
 
@@ -153,9 +153,10 @@ INSERT INTO `icash` (`id_icash`, `id_user`, `saldo_icash`) VALUES
 (9, 1027, 960000),
 (10, 1028, 1065000),
 (11, 1028, 1065000),
-(12, 1029, 705000),
+(12, 1029, 440000),
 (13, 1030, 0),
-(14, 1031, 0);
+(14, 1031, 0),
+(15, 1032, 810000);
 
 -- --------------------------------------------------------
 
@@ -177,16 +178,30 @@ CREATE TABLE `jadwal` (
 --
 
 INSERT INTO `jadwal` (`id_jadwal`, `id_film`, `tgl_jadwal`, `id_jamtayang`, `id_harga`, `id_studio`) VALUES
-('5db654785e2', '24', '29-10-2019', 'T003', 'M001', 'S001'),
-('5db921c0708', '24', '12-12-2012', 'T001', 'M002', 'S001'),
+('5db654785e2', '5dbd905aae457', '2019-10-29', 'T003', 'M001', 'S001'),
+('5db921c0708', '5dbd905aae457', '2012-12-12', 'T001', 'M002', 'S001'),
 ('5db923d93d4', '23', '2019-12-03', 'T003', 'M002', 'S001'),
-('5dbd9661ae9', '5dbd905aae457', '27-04-2019', 'T001', 'M001', 'S002'),
-('5dc2b7e548a', '5dbd905aae457', '2019-11-07', 'T003', 'M001', 'S001'),
+('5dbd9661ae9', '5dbd905aae457', '2019-12-03', 'T001', 'M001', 'S002'),
+('5dc2b7e548a', '5dbd905aae457', '2019-12-26', 'T003', 'M001', 'S001'),
 ('5dc3ad11afb', '5dbd905aae457', '2019-11-04', 'T001', 'M002', 'S001'),
-('j001', '23', '10-10-2019', 'T001', 'M002', 'S001'),
-('j002', '23', '10-10-2019', 'T002', 'M002', 'S001'),
-('j003', '24', '10-10-2019', 'T002', 'M002', 'S002'),
+('5dfc3e20be6', '23', '2019-12-25', 'T002', 'M001', 'S001'),
+('5e00536e196', '23', '2019-12-24', 'T002', 'M001', 'S001'),
+('5e04d81f01f', '5dbd905aae457', '2019-12-31', 'T002', 'M001', 'S001'),
+('5e0d77ca0a2', '23', '2020-01-02', 'T002', 'M001', 'S001'),
+('j001', '23', '2019-12-26', 'T001', 'M002', 'S001'),
+('j002', '23', '2019-12-31', 'T002', 'M002', 'S001'),
+('j003', '23', '2019-10-10', 'T002', 'M002', 'S002'),
 ('j004', '23', '2019-12-02', 'T002', 'M001', 'S002');
+
+--
+-- Trigger `jadwal`
+--
+DELIMITER $$
+CREATE TRIGGER `jadwal` AFTER INSERT ON `jadwal` FOR EACH ROW BEGIN
+ insert recort set id_jadwal = new.id_jadwal;
+ END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -314,7 +329,18 @@ CREATE TABLE `nonton` (
 INSERT INTO `nonton` (`id_nonton`, `id_pesan`, `kursi`) VALUES
 (5, '5dd14432a4e7f', 'C3'),
 (8, '5dd6120010041', 'D2'),
-(9, '5dd6120010041', 'D2 D3');
+(9, '5dd6120010041', 'D2 D3'),
+(10, '5dfbfd19d849c', 'F5'),
+(11, '5dfc3e7cf3c99', 'A2'),
+(12, '5dd141db87757', 'B2'),
+(13, '5dd141db87757', 'B2 B4'),
+(14, '5e01c1b0c95de', 'A1 B2'),
+(15, '5e01c1b0c95de', 'A1 A2 B2'),
+(16, '5dd141db87757', 'B2 B3 B4'),
+(17, '5e005d2dedcf3', 'E4'),
+(18, '5e005d2dedcf3', 'E4 F4'),
+(19, '5e005d2dedcf3', 'E4 F4'),
+(20, '5e005d2dedcf3', 'E4 F4');
 
 -- --------------------------------------------------------
 
@@ -337,7 +363,15 @@ INSERT INTO `pengeluaran` (`id_keluar`, `tgl`, `id_user`, `id_pesan`) VALUES
 (44, '2019-11-17 12:49:31', 1029, '5dd141db87757'),
 (46, '2019-11-17 13:01:32', 1029, '5dd144accf695'),
 (47, '2019-11-21 04:26:40', 1029, '5dd6120010041'),
-(48, '2019-11-24 13:40:22', 1029, '5dda884680f81');
+(48, '2019-11-24 13:40:22', 1029, '5dda884680f81'),
+(49, '2019-12-19 14:39:31', 1029, '5dfb8ba3a337a'),
+(50, '2019-12-20 03:17:34', 1032, '5dfc3d4e2067e'),
+(51, '2019-12-20 03:22:36', 1032, '5dfc3e7cf3c99'),
+(57, '2019-12-23 06:01:34', 2, '1'),
+(58, '2019-12-23 06:02:05', 1029, '5e00585d6e88e'),
+(60, '2019-12-23 06:22:37', 1029, '5e005d2dedcf3'),
+(61, '2019-12-23 06:24:03', 1029, '5e005d83a8f46'),
+(62, '2019-12-24 07:43:44', 1029, '5e01c1b0c95de');
 
 -- --------------------------------------------------------
 
@@ -362,12 +396,21 @@ CREATE TABLE `pesan` (
 --
 
 INSERT INTO `pesan` (`id_pesan`, `id_user`, `tanggal_pesan`, `id_film`, `id_jadwal`, `id_kursi`, `jumlah_pesanan`, `total_harga`, `id_status`) VALUES
-('5dd141db87757', 1029, '2019-11-25 11:51:43', '5dbd905aae457', '5dbd9661ae9', 'B2 B3 B4', 2, 70000, 3),
-('5dd1428b93fb7', 1029, '2019-11-25 11:49:59', '23', '5db923d93d4', 'D3', 1, 35000, 3),
+('1', 2, '2019-12-28 08:46:32', '23', '5e00536e196', 'B1', 1, 35000, 2),
+('5dd141db87757', 1029, '2020-01-02 05:32:05', '23', '5db923d93d4', 'B2 B3 B4', 2, 70000, 3),
+('5dd1428b93fb7', 1029, '2019-12-19 17:00:00', '23', '5db923d93d4', 'D3', 1, 35000, 1),
 ('5dd14432a4e7f', 1029, '2019-11-19 07:26:16', '5dbd905aae457', '5dbd9661ae9', 'C3', 1, 35000, 1),
 ('5dd144accf695', 1029, '2019-11-17 13:01:50', '23', '5db923d93d4', 'E4', 1, 35000, 3),
 ('5dd6120010041', 1029, '2019-11-25 11:44:49', '23', 'j002', 'D1 D2 D3', 3, 120000, 3),
-('5dda884680f81', 1029, '2019-11-25 12:16:04', '23', 'j004', 'D1', 1, 35000, 3);
+('5dda884680f81', 1029, '2019-12-19 22:37:23', '23', 'j004', 'D1', 1, 35000, 1),
+('5dfb8ba3a337a', 1029, '2019-12-19 22:37:23', '5dbd905aae457', '5dc2b7e548a', 'E4', 1, 35000, 1),
+('5dfbfd19d849c', 1029, '2019-12-19 22:44:31', '23', '5db923d93d4', 'F5', 1, 40000, 3),
+('5dfc3d4e2067e', 1032, '2019-12-20 03:17:34', '23', '5db923d93d4', 'B3 B4 C4', 3, 120000, 1),
+('5dfc3e7cf3c99', 1032, '2019-12-20 03:32:22', '23', '5dfc3e20be6', 'A2 B2', 2, 70000, 3),
+('5e00585d6e88e', 1029, '2019-12-23 06:02:05', '23', '5e00536e196', 'C2 C3', 2, 70000, 1),
+('5e005d2dedcf3', 1029, '2020-01-02 07:43:31', '23', '5e00536e196', 'E4 F4', 2, 70000, 3),
+('5e005d83a8f46', 1029, '2019-12-23 06:24:03', '23', '5e00536e196', 'E5', 1, 35000, 1),
+('5e01c1b0c95de', 1029, '2019-12-24 07:44:18', '5dbd905aae457', '5dc2b7e548a', 'A1 A2 B2', 3, 105000, 3);
 
 --
 -- Trigger `pesan`
@@ -379,6 +422,7 @@ INSERT INTO pengeluaran set id_pesan= new.id_pesan, id_user= new.id_user;
  update icash set saldo_icash = saldo_icash - new.total_harga
  where id_user= new.id_user;
   end if;
+  update recort set j_pesanan = (select COUNT(id_pesan) from pesan where id_jadwal= new.id_jadwal), j_kursi= (select SUM(jumlah_pesanan) from pesan where id_jadwal= new.id_jadwal),j_kursib=((select count(id_kursi) from kursi)-(select SUM(jumlah_pesanan) from pesan where id_jadwal= new.id_jadwal)) ,pemasukkan = (SELECT SUM(total_harga) from pesan WHERE id_jadwal=new.id_jadwal) where id_jadwal= new.id_jadwal;
 END
 $$
 DELIMITER ;
@@ -406,7 +450,8 @@ INSERT INTO `rating` (`id_rating`, `tanggal`, `id_user`, `id_film`, `nilai`, `ul
 (50, '2019-11-17 13:05:08', 1029, '23', '8.5', 'keren'),
 (51, '2019-11-26 14:09:10', 1029, '23', '9.5', 'seru'),
 (52, '2019-11-26 14:09:59', 1029, '5dbd905aae457', '4.5', 'oke'),
-(53, '2019-11-26 14:12:56', 1029, '23', '3.5', '');
+(53, '2019-11-26 14:12:56', 1029, '23', '3.5', ''),
+(54, '2019-12-14 09:26:57', 1029, '23', '5.0', 'y');
 
 --
 -- Trigger `rating`
@@ -418,6 +463,41 @@ CREATE TRIGGER `rating` AFTER INSERT ON `rating` FOR EACH ROW BEGIN
  END
 $$
 DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `recort`
+--
+
+CREATE TABLE `recort` (
+  `id_recort` int(11) NOT NULL,
+  `id_jadwal` varchar(11) NOT NULL,
+  `j_pesanan` int(11) NOT NULL,
+  `j_kursi` int(11) NOT NULL,
+  `j_kursib` int(11) NOT NULL,
+  `pemasukkan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `recort`
+--
+
+INSERT INTO `recort` (`id_recort`, `id_jadwal`, `j_pesanan`, `j_kursi`, `j_kursib`, `pemasukkan`) VALUES
+(1, '5e00536e196', 4, 6, 64, 210000),
+(2, '5db654785e2', 0, 0, 0, 0),
+(3, '5db921c0708', 0, 0, 0, 0),
+(4, '5db923d93d4', 0, 0, 0, 0),
+(5, '5dbd9661ae9', 0, 0, 0, 0),
+(6, '5dc2b7e548a', 0, 0, 0, 0),
+(7, '5dc3ad11afb', 0, 0, 0, 0),
+(8, '5dfc3e20be6', 0, 0, 0, 0),
+(9, 'j001', 0, 0, 0, 0),
+(10, 'j002', 0, 0, 0, 0),
+(11, 'j003', 0, 0, 0, 0),
+(12, 'j004', 0, 0, 0, 0),
+(13, '5e04d81f01f', 0, 0, 0, 0),
+(14, '5e0d77ca0a2', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -485,7 +565,10 @@ INSERT INTO `topup` (`id_topup`, `tanggal`, `id_user`, `id_bank`, `jumlah_transf
 (64, '2019-11-17 19:48:16', 1029, '5d880db876ad5', 1000000, 'machrus', '2019-11-17 20:48:16', '1'),
 (65, '2019-11-17 21:27:54', 1029, '5d8810b053e6b', 50000, 'machrus', '2019-11-17 22:27:54', '4'),
 (66, '2019-11-20 12:22:03', 1029, '5d881319f0a4a', 25000, 'ggg', '2019-11-20 13:22:03', '4'),
-(67, '2019-11-24 20:40:36', 1029, '5d8810b053e6b', 10000, 'machrus', '2019-11-24 21:40:36', '4');
+(67, '2019-11-24 20:40:36', 1029, '5d8810b053e6b', 10000, 'machrus', '2019-11-24 21:40:36', '4'),
+(68, '2019-12-20 05:46:47', 1029, '5d880db876ad5', 25000, 'g', '2019-12-20 06:46:47', '1'),
+(69, '2019-12-20 05:51:26', 1029, '5d880db876ad5', 25000, 'y', '2019-12-20 06:51:26', '1'),
+(70, '2019-12-20 10:15:16', 1032, '5d880db876ad5', 1000000, 'machrus', '2019-12-20 11:15:16', '1');
 
 --
 -- Trigger `topup`
@@ -522,7 +605,8 @@ CREATE TABLE `transfer` (
 
 INSERT INTO `transfer` (`id_transfer`, `id_user`, `id_pesan`, `id_bank`, `nama`, `bts_transfer`, `id_status`) VALUES
 (7, 1029, '5dd1428b93fb7', '5d8810b053e6b', 'machrus ', '2019-11-17 20:52:22', 1),
-(8, 1029, '5dd14432a4e7f', '5d88134403ceb', 'machrus ', '2019-11-17 20:59:25', 4);
+(8, 1029, '5dd14432a4e7f', '5d88134403ceb', 'machrus ', '2019-11-17 20:59:25', 4),
+(9, 1029, '5dfbfd19d849c', '5d881319f0a4a', 'hh', '2019-12-20 06:43:35', 1);
 
 --
 -- Trigger `transfer`
@@ -567,7 +651,8 @@ INSERT INTO `user` (`id_user`, `username`, `alamat`, `email`, `no_tlp`, `passwor
 (1026, 'Alfan Fatoni', 'salamrejo', 'alfanfatoni92@gmail.com', '08983784759', '25d55ad283aa400af464c76d713c07ad', '2', 'IMG_20191019_115308.jpg'),
 (1029, 'machrus', 'Banyuwangi', 'machrus@gmail.com', '08123456789', '202cb962ac59075b964b07152d234b70', '2', '1569543611715.jpg'),
 (1030, 'aan', 'Kediri', 'aanmachrus@gmail.com', '089660652230', 'c20ad4d76fe97759aa27a0c99bff6710', '2', NULL),
-(1031, 'danang', 'banyuwangi', 'danang@gamil.com', '0899999', '6a17faad3b1275fd2558d5435c58440e', '2', NULL);
+(1031, 'danang', 'banyuwangi', 'danang@gamil.com', '0899999', '6a17faad3b1275fd2558d5435c58440e', '2', NULL),
+(1032, 'aanmachrus', 'banyuwangi', 'aanmachrus@gmail.com', '08123456789', 'c20ad4d76fe97759aa27a0c99bff6710', '2', NULL);
 
 --
 -- Trigger `user`
@@ -657,6 +742,12 @@ ALTER TABLE `rating`
   ADD PRIMARY KEY (`id_rating`);
 
 --
+-- Indeks untuk tabel `recort`
+--
+ALTER TABLE `recort`
+  ADD PRIMARY KEY (`id_recort`);
+
+--
 -- Indeks untuk tabel `status`
 --
 ALTER TABLE `status`
@@ -694,43 +785,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `icash`
 --
 ALTER TABLE `icash`
-  MODIFY `id_icash` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_icash` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT untuk tabel `nonton`
 --
 ALTER TABLE `nonton`
-  MODIFY `id_nonton` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_nonton` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengeluaran`
 --
 ALTER TABLE `pengeluaran`
-  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT untuk tabel `rating`
 --
 ALTER TABLE `rating`
-  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id_rating` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+
+--
+-- AUTO_INCREMENT untuk tabel `recort`
+--
+ALTER TABLE `recort`
+  MODIFY `id_recort` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT untuk tabel `topup`
 --
 ALTER TABLE `topup`
-  MODIFY `id_topup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id_topup` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT untuk tabel `transfer`
 --
 ALTER TABLE `transfer`
-  MODIFY `id_transfer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_transfer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1032;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1033;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
